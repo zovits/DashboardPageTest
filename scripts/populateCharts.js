256cbf94f0a40a8a232e0ea8748f8c57ba701462
zovits <zovits@roblox.com>
2022-07-14 10:32:59 -0700

    Commit timestamps and todo charts

--- scripts/populateCharts.js
+++ scripts/populateCharts.js
@@ -35,16 +35,18 @@ readTextFile("data/code-coverage.json", function(coverageReport){
 	};
 
 	for (label in rawData) {
-		labels.push(label);
-		linesDataset.data.push(rawData[label].lines);
-		funcsDataset.data.push(rawData[label].functions);
+		const datapoint = rawData[label];
+		const date = new Date(datapoint.timestamp*1000);
+		labels.push(`${label} - ${date.toLocaleDateString()}`);
+		linesDataset.data.push(datapoint.lines);
+		funcsDataset.data.push(datapoint.functions);
 		linesDataset.backgroundColor.push(barColors[0]);
 		funcsDataset.backgroundColor.push(barColors[1]);
 	}
 
 
-	const coverageCtx = document.getElementById('codeCoverage').getContext('2d');
-	const coverageChart = new Chart(coverageCtx, {
+	const codeCoverageCtx = document.getElementById('codeCoverage').getContext('2d');
+	const codeCoverageChart = new Chart(codeCoverageCtx, {
 		type: 'bar',
 		data: {
 			labels: labels,
@@ -55,17 +57,92 @@ readTextFile("data/code-coverage.json", function(coverageReport){
 				y: {
 					min: 0, max: 100,
 				}
+			},
+		},
+	});
+});
+
+// Type Coverage
+const typeCoverageCtx = document.getElementById('typeCoverage').getContext('2d');
+const typeCoverageChart = new Chart(typeCoverageCtx, {
+	type: 'bar',
+	data: {
+		labels: ["TODO"],
+		datasets: [{
+			label: 'TODO',
+			borderWidth: 0,
+			data: [0],
+			backgroundColor: [barColors[2]],
+		}],
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0, max: 100,
 			}
 		},
-		plugins: {
-            tooltip: {
-                callbacks: {
-                    label: function(context) {
-                        console.log(context);
-                        return context.dataset.label;
-                    }
-                }
-            },
-        },
-	});
+	},
+});
+// Flag Combinatorics
+const flagCombinatoricsCtx = document.getElementById('flagCombinatorics').getContext('2d');
+const flagCombinatoricsChart = new Chart(flagCombinatoricsCtx, {
+	type: 'bar',
+	data: {
+		labels: ["TODO"],
+		datasets: [{
+			label: 'TODO',
+			borderWidth: 0,
+			data: [0],
+			backgroundColor: [barColors[2]],
+		}],
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0, max: 100,
+			}
+		}
+	},
+});
+// Cyclomatic Complexity
+const cyclomaticComplexityCtx = document.getElementById('cyclomaticComplexity').getContext('2d');
+const cyclomaticComplexityChart = new Chart(cyclomaticComplexityCtx, {
+	type: 'bar',
+	data: {
+		labels: ["TODO"],
+		datasets: [{
+			label: 'TODO',
+			borderWidth: 0,
+			data: [0],
+			backgroundColor: [barColors[2]],
+		}],
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0, max: 100,
+			}
+		}
+	},
+});
+// Code Duplication
+const codeDuplicationCtx = document.getElementById('codeDuplication').getContext('2d');
+const codeDuplicationChart = new Chart(codeDuplicationCtx, {
+	type: 'bar',
+	data: {
+		labels: ["TODO"],
+		datasets: [{
+			label: 'TODO',
+			borderWidth: 0,
+			data: [0],
+			backgroundColor: [barColors[2]],
+		}],
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0, max: 100,
+			}
+		}
+	},
 });
